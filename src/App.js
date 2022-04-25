5297820954ae0e1563c648b039f628be29e6fb33
Gerardo Santos Gonzalez <97237961+gerardo-santos@users.noreply.github.com>
2022-04-25 16:49:59 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import MicRecorder from 'mic-recorder-to-mp3';
 
-function App() {
+const App = () => {
+  const [isRecording, setIsRecording] = useState(false);
+  const [blobUrl, setBlobUrl] = useState('');
+  const [isBlocked, setIsBlocked] = useState(false);
+  const [mp3Recorder, setMp3Recorder] = useState(new MicRecorder({ bitRate: 128 }));
+
+  console.log(setMp3Recorder);
+
+  useEffect(() => {
+    navigator.getUserMedia({ audio: true },
+      () => {
+        console.log('Permission Granted');
+        setIsBlocked(false);
+      },
+      () => {
+        console.log('Permission Denied');
+        setIsBlocked(true);
+      },
+    );
+  }, [])
+
+  const start = () => {
+    if (isBlocked) {
+      console.log('Permission Denied');
+    } else {
+      mp3Recorder
+        .start()
+        .then(() => {
+          setIsRecording(true)
+        }).catch((e) => console.error(e));
+    }
+  }
+
+  const stop = () => {
+    mp3Recorder
+    .stop()
+    .getMp3()
+    .then(([buffer, blob]) => {
+      const blobURL = URL.createObjectURL(blob)
+      setBlobUrl(blobURL)
+      setIsRecording(false)
+    }).catch((e) => console.log(e));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>hola</h1>
+      <button onClick={start} disabled={isRecording}>
+        Record
+      </button>
+      <button onClick={stop} disabled={!isRecording}>
+        Stop
+      </button>
+      <audio src={blobUrl} controls="controls" />
     </div>
   );
 }
 
 export default App;
+
